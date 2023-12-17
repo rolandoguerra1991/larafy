962e72b324f0f52bf5fb4a7a35e2c04c59bfc8df
rolando guerra <rolandoguerra1991@gmail.com>
2023-12-16 22:06:06 -0400

    vuetify integrated

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,49 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue'
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import { fileURLToPath, URL } from 'node:url'
+import fs from 'node:fs';
+
+const host = 'larafy.test';
 
 export default defineConfig({
+    server: {
+        host,
+        hmr: { host },
+        https: {
+            key: fs.readFileSync(`C:/laragon/etc/ssl/laragon.key`),
+            cert: fs.readFileSync(`C:/laragon/etc/ssl/laragon.crt`),
+        },
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/js/app.js'],
             refresh: true,
         }),
+        vue({
+            template: { transformAssetUrls }
+        }),
+        // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+        vuetify({
+            autoImport: true,
+            styles: {
+                configFile: 'resources/scss/app.scss',
+            },
+        }),
     ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url))
+        },
+        extensions: [
+            '.js',
+            '.json',
+            '.jsx',
+            '.mjs',
+            '.ts',
+            '.tsx',
+            '.vue',
+        ],
+    },
 });
